f8b9fbac7b130cef9b147fdd6b5a2fd35116ff54
HamidGuluzadeh <hamidguluzadeh@gmail.com>
2026-07-01 12:25:48 +0400

    Improving program for taking input from user

--- Level-1/Calculator-Program/src/Main.java
+++ Level-1/Calculator-Program/src/Main.java
@@ -1,6 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        Calculator calculator = new Calculator(10.5, 7.3);
+        Scanner scan = new Scanner(System.in);
+
+        Calculator calculator = new Calculator();
+
+        System.out.print("Enter first number: ");
+        double firstNumber = scan.nextDouble();
+        System.out.print("Enter second number: ");
+        double secondNumber = scan.nextDouble();
+
+        calculator.setFirstNumber(firstNumber);
+        calculator.setSecondNumber(secondNumber);
 
         System.out.println("Addition: " + calculator.addition());
         System.out.println("Subtraction: " + calculator.subtraction());
