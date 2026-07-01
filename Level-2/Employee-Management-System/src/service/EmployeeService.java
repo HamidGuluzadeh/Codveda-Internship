acfadd2bd553057ccfd2790a32d5391441c53c92
HamidGuluzadeh <hamidguluzadeh@gmail.com>
2026-07-01 15:22:21 +0400

    Creating EmployeeService interface related to EmployeeServiceImpl class

--- Level-2/Employee-Management-System/src/service/EmployeeService.java
+++ Level-2/Employee-Management-System/src/service/EmployeeService.java
@@ -1,87 +1,17 @@
 package service;
 
 import entity.Employee;
-import exception.EmployeeNotFoundException;
-import exception.EmptyDataException;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
+public interface EmployeeService {
 
-public class EmployeeService {
-    private static final List<Employee> employees = new ArrayList<>();
+    void addEmployee(Employee employee);
 
-    public void addEmployee(Employee employee) {
-        employees.add(employee);
-        System.out.println("Employee added!");
-    }
+    void viewAllEmployees();
 
-    public void viewAllEmployees() {
-        if (employees.isEmpty()) {
-            System.err.println("No employees found!");
-            return;
-        }
+    void viewEmployeeById(Integer id);
 
-        for (Employee employee : employees) {
-            System.out.println(employee);
-        }
-    }
+    void updateEmployee(Employee employee, Integer id);
 
-    public void viewEmployeeById(Integer id) {
-        if (employees.isEmpty()) {
-            System.err.println("No employees found!");
-        }
+    void deleteEmployee(Integer id);
 
-        for (Employee employee : employees) {
-            if (Objects.equals(employee.getId(), id)) {
-                System.out.println(employee);
-                return;
-            }
-        }
-
-        throw new EmployeeNotFoundException("Employee not found!");
-    }
-
-    public void updateEmployee(Employee updatedEmployee, Integer id) {
-        if (employees.isEmpty()) {
-            System.err.println("No employees found!");
-        }
-
-        if (Objects.isNull(updatedEmployee)) {
-            throw new EmptyDataException("Request data cannot be empty!");
-        }
-
-        for (Employee employee : employees) {
-            if (Objects.equals(employee.getId(), id)) {
-                if (!Objects.isNull(updatedEmployee.getFirstName())) {
-                    employee.setFirstName(updatedEmployee.getFirstName());
-                } if (!Objects.isNull(updatedEmployee.getLastName())) {
-                    employee.setLastName(updatedEmployee.getLastName());
-                } if (!Objects.isNull(updatedEmployee.getAge())) {
-                    employee.setAge(updatedEmployee.getAge());
-                } if (!Objects.isNull(updatedEmployee.getPhoneNumber())) {
-                    employee.setPhoneNumber(updatedEmployee.getPhoneNumber());
-                } if (!Objects.isNull(updatedEmployee.getPosition())) {
-                    employee.setPosition(updatedEmployee.getPosition());
-                } if (!Objects.isNull(updatedEmployee.getSalary())) {
-                    employee.setSalary(updatedEmployee.getSalary());
-                }
-
-                System.out.println(employee);
-                System.out.println("Employee updated!");
-
-                return;
-            }
-        }
-    }
-
-    public void deleteEmployee(Integer id) {
-        boolean isRemoved = employees.removeIf(employee -> Objects.equals(employee.getId(), id));
-
-        if (isRemoved) {
-            System.out.println("Employee deleted!");
-        } else {
-            throw new EmployeeNotFoundException("Employee not found!");
-        }
-    }
 }
